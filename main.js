eed86dc6223cef55e2ab9b5ccb1f5a65d68bd1af
Barbue <david.crochet@codeimmersives.com>
2022-10-24 15:19:17 -0500

    update index.html and main.js files

---
+++ main.js
@@ -0,0 +1,39 @@
+// Changethe font color of the paragraph
+
+let para = document.querySelector('p');
+
+para.style.color ='lightblue';
+
+//Change the font size of the heading to 48px.
+
+let head = document.querySelector('h1');
+
+head.style.fontSize ='48px';
+
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let item13 = document.querySelector('#item-13');
+
+item13.style.opacity = '0.5';
+
+let item3 = document.querySelector('#item-3');
+
+item3.innerText = 'I say, Hi!';
+
+let myImage = document.querySelector('img');
+
+myImage.src = 'https://images.immediate.co.uk/production/volatile/sites/4/2021/08/mountains-7ddde89.jpg?quality=90&webp=true&resize=1200,510';
+
+myImage.style.height = '300px';
+
+let image2 = document.querySelector('#IMG');
+
+image2.style.height = '300px'
+
+let item16 = document.querySelector('#item-16');
+
+item16.className = 'item';
+
+item16.innerText = "Won't get fooled again."
+
+
